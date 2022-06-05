7a516adf20377823bcfb87464d10e6ead2d5b599
Dominic DiGiacomo <dominic.m.digiacomo@gmail.com>
2022-06-05 03:01:31 -0400

    clean

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,21 +17,14 @@ import { AppProps } from "next/app";
 import { FC, useMemo } from "react";
 import { GlobalStyle } from "../src/utils/styleKit";
 
-// Use require instead of import since order matters
 require("@solana/wallet-adapter-react-ui/styles.css");
 require("../styles/globals.css");
 require("../styles/wallet_adapter.css");
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
-  // Can be set to 'devnet', 'testnet', or 'mainnet-beta'
   const network = WalletAdapterNetwork.Devnet;
-
-  // You can also provide a custom RPC endpoint
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
-  // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
-  // Only the wallets you configure here will be compiled into your application, and only the dependencies
-  // of wallets that your users connect to will be loaded
   const wallets = useMemo(
     () => [
       new PhantomWalletAdapter(),
@@ -61,4 +54,3 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
 };
 
 export default App;
-
